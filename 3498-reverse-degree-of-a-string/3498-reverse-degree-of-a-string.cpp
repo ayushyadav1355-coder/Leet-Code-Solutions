1c38740a7e716e4d33d099a3185421f2835e79b1
Ayush Yadav <ayushyadav1355@gmail.com>
2026-09-20 15:35:45 +0530

    Time: 0 ms (100%), Space: 9.7 MB (78.4%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -0,0 +1,14 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int ans = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            ans += ('z' - s[i] + 1) * (i + 1);
+        }
+        return ans;
+    }
+};
